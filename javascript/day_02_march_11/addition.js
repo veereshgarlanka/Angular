d93a5c98da35e005ffba3dd5a2bbc323bbcc25e6
veeresh <saiveeresh.garlanka@gmail.com>
2025-03-11 15:34:11 -0400

    Modified

--- javascript/day_02_march_11/addition.js
+++ javascript/day_02_march_11/addition.js
@@ -3,4 +3,4 @@ let b = 20;
 console.log(`Addition of ${a} & ${b} is ${a + b}`);
 
 // alert(`Addition of ${a} & ${b} is ${a+b}`); // works only in browser
-// document.write(`Addition of ${a} & ${b} is ${a+b}`); // works only in browser
+// document.write(`Addition of ${a} & ${b} is ${a+b}`); // works only in browser//
